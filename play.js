7aa9e9170602277608d287be5628f8b0f0dc0ec0
adilmhd4539 <adil@innsof.com>
2024-04-09 09:46:53 +0530

    learned using promises

--- play.js
+++ play.js
@@ -79,3 +79,22 @@ setTimeout(()=>{
 
 },1000);
 console.log('executed before set timeout');
+//using promises
+const fetchData=()=>{
+    const promise=new Promise((resolve,reject)=>{
+setTimeout(()=>{
+        resolve('Done!');
+    },1500);
+    });
+    return promise;
+    
+};
+setTimeout(()=>{
+    console.log('Timer is done');
+    fetchData().then(text=>{
+        console.log(text);
+        return fetchData();
+    }).then(text2=>{
+        console.log(text2);
+    });
+},2000);
